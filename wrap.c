56a83718d5e507f897fc110664cba2f2671ec7d3
dreamca4er <dreamca4er1@gmail.com>
2013-05-06 01:04:54 +0700

    Comm ops start time

--- wrap.c
+++ wrap.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "sys/time.h"
 #include "time.h"
+
 typedef unsigned long int int32_t;
 typedef const char* chpl_string;
 
@@ -12,16 +13,22 @@ void __wrap_chpl_comm_get(void *addr, int32_t locale, void* raddr,
                           int32_t elemSize, int32_t typeIndex, int32_t len,
                           int ln, chpl_string fn)
 {
-  struct timeval st, fin, time;
+  struct timeval st, fin, tim;
+  time_t rawtime;
+  struct tm* info;
   FILE* f;
   f = fopen("results/chpl_comm_get", "a");
+  time(&rawtime);
   gettimeofday(&st, NULL);
   __real_chpl_comm_get(addr, locale, raddr, elemSize,
 		       typeIndex, len, ln, fn);
   gettimeofday(&fin, NULL);
-  time.tv_sec = fin.tv_sec - st.tv_sec;
-  time.tv_usec = fin.tv_usec - st.tv_usec;
-  fprintf(f, "chpl_comm_get from locale %d, %ld.%ld s\n",locale, time.tv_sec, time.tv_usec);
+  tim.tv_sec = fin.tv_sec - st.tv_sec;
+  tim.tv_usec = fin.tv_usec - st.tv_usec;
+  info = localtime(&rawtime);
+  fprintf(f, "chpl_comm_get from loc %d, %ld.%ld s, start at %d:%d:%d\n",
+	  locale, tim.tv_sec, tim.tv_usec,
+          info->tm_hour, info->tm_min, info->tm_sec);
   fclose(f);
   return;
 }
@@ -34,16 +41,23 @@ void __wrap_chpl_comm_put(void *addr, int32_t locale, void* raddr,
                           int32_t elemSize, int32_t typeIndex, int32_t len,
                           int ln, chpl_string fn)
 {
-  struct timeval st, fin, time;
+  struct timeval st, fin, tim;
+  time_t rawtime;
+  struct tm* info;
   FILE* f;
   f = fopen("results/chpl_comm_put", "a");
+  time(&rawtime);
   gettimeofday(&st, NULL);
   __real_chpl_comm_put(addr, locale, raddr, elemSize,
                        typeIndex, len, ln, fn);
   gettimeofday(&fin, NULL);
-  time.tv_sec = fin.tv_sec - st.tv_sec;
-  time.tv_usec = fin.tv_usec - st.tv_usec;
-  fprintf(f, "chpl_comm_put from locale %d, %ld.%ld s\n",locale, time.tv_sec, time.tv_usec);
+  tim.tv_sec = fin.tv_sec - st.tv_sec;
+  tim.tv_usec = fin.tv_usec - st.tv_usec;
+  info = localtime(&rawtime);
+  fprintf(f, "chpl_comm_put to loc %d, %ld.%ld s, start at %d:%d:%d\n",
+          locale, tim.tv_sec, tim.tv_usec,
+          info->tm_hour, info->tm_min, info->tm_sec);
+
   fclose(f);
   return;
 }
