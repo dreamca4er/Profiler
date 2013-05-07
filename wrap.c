ce58f8e1ed919d6b93b4f6ce73daee2390b93372
dreamca4er <dreamca4er1@gmail.com>
2013-05-07 16:01:37 +0700

    Getting full info

--- wrap.c
+++ wrap.c
@@ -6,6 +6,7 @@ typedef unsigned long int int32_t;
 typedef const char* chpl_string;
 
 struct timeval prog_start;
+extern int chpl_localeID;
 
 void __real_chpl_comm_init(int *argc_p, char ***argv_p);
 
@@ -35,8 +36,10 @@ void __wrap_chpl_comm_get(void *addr, int32_t locale, void* raddr,
   tim.tv_usec = fin.tv_usec - st.tv_usec;
   time_from_st = (double)(st.tv_sec - prog_start.tv_sec);
   time_from_st += (st.tv_usec - prog_start.tv_usec) * 0.000001;
-  fprintf(f, "get from loc %d, %ld.%ld s, %.3lf s from init\n",
-	  locale, tim.tv_sec, tim.tv_usec,time_from_st );
+  fprintf(f, "%d gets from %d, %ld.%ld s, %.3lf s from init, %s\n",
+	  chpl_localeID, locale, tim.tv_sec, tim.tv_usec, time_from_st, fn);
+  /*fprintf(f, "locale = %d, elemSize = %d, typeIndex = %d, len = %d, ln = %d, chpl_ctring = %s\n",
+          locale, elemSize, typeIndex, len, ln, fn);*/
   fclose(f);
   return;
 }
@@ -61,8 +64,8 @@ void __wrap_chpl_comm_put(void *addr, int32_t locale, void* raddr,
   tim.tv_usec = fin.tv_usec - st.tv_usec;
   time_from_st = (double)(st.tv_sec - prog_start.tv_sec);
   time_from_st += (st.tv_usec - prog_start.tv_usec) * 0.000001;
-  fprintf(f, "put to loc %d, %ld.%ld s, %.3lf s from init\n",
-          locale, tim.tv_sec, tim.tv_usec, time_from_st);
+  fprintf(f, "%d puts to %d, %ld.%ld s, %.3lf s from init, %s\n",
+          chpl_localeID, locale, tim.tv_sec, tim.tv_usec, time_from_st, fn);
 
   fclose(f);
   return;
