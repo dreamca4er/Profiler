307f080cd8fad90c5923b446ecdc1dfe495a6af1
dreamca4er <dreamca4er1@gmail.com>
2013-05-10 22:59:08 +0700

    Basic timeline

--- graph.c
+++ graph.c
@@ -9,19 +9,34 @@ typedef struct op{
   int put;
 }oper;
 
+typedef struct act{
+  int from;
+  int to;
+  char *op;
+  float exec;
+  float time;
+  char *module;
+}action;
+
 int main(int argc, char** argv)
 {
-  int count = 0, numlocs, i, j, numgets = 0, numputs = 0, col = 0;
-  char info[100], to[100], comm_op[100];
-  FILE *f, *graph, *nl, *matrix;
+  int numlocs, i, j, numgets = 0,
+      numputs = 0, col = 0;
+  char info[100], to[100], comm_op[100], buf[100];
+  float end_time, max_time = 0.0;
+  FILE *f, *graph, *nl, *matrix, *timeline;
   oper **ops;
+  action ac; //structure for drawing ops
   f = fopen("results/m", "r");
   graph = fopen("results/graph.gv", "w");
   nl = fopen("results/info", "r");
   matrix = fopen("results/matrix.html", "w");
+  timeline = fopen("results/timeline.html", "w");
   fscanf(nl, "%s", info);
-  fclose(nl);
   numlocs = atoi(info);
+  fscanf(nl, "%s", info);
+  end_time = atof(info);
+  fclose(nl);
   ops = (oper **)malloc(numlocs * sizeof(oper *));
   for(i = 0; i < numlocs; i++)
     ops[i] = (oper *)malloc(numlocs * sizeof(oper));
@@ -33,32 +48,82 @@ int main(int argc, char** argv)
   fprintf(graph, "digraph gr{\n");
   fprintf(graph, "  node [height = 1, width = 1.4];\n");
 
-  while(1){
-    count++;
-    fscanf(f, "%s", info);
+  fprintf(timeline, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+  fprintf(timeline, "<svg style=\"background: white\" version = \"1.1\"\n");
+  fprintf(timeline, "baseProfile=\"full\"\n");
+  fprintf(timeline, "xmlns = \"http://www.w3.org/2000/svg\" \n");
+  fprintf(timeline, "xmlns:xlink = \"http://www.w3.org/1999/xlink\" \n");
+  fprintf(timeline, "xmlns:ev = \"http://www.w3.org/2001/xml-events\" \n");
+  fprintf(timeline, "height = \"%dpx\" width = \"800px\">\n", cl * (numlocs + 1) + 150);
 
+  while(1){
+    fscanf(f, "%s", info);
     if(feof(f))
       break;
-    if(count % 10 == 1){
-      fprintf(graph, "  loc%s -> ", info); count++;
-      fscanf(f, "%s", to);
-      fprintf(graph, "loc%s ", to); count++;
-      fscanf(f, "%s", comm_op);
-      fprintf(graph, "[ label = \"%s\" ];\n", comm_op);
-      if(!strcmp(comm_op, "get")){
-        ops[ atoi(info) ][ atoi(to) ].get++;
-        numgets++;
-      }
-      else{
-        ops[ atoi(info) ][ atoi(to) ].put++;
-        numputs++;
-        }
+
+    ac.from = atoi(info);
+    fprintf(graph, "  loc%s -> ", info);
+    fscanf(f, "%s", to);
+    ac.to = atoi(to);
+    fprintf(graph, "loc%s ", to);
+    fscanf(f, "%s", comm_op);
+    ac.op = comm_op;
+    fprintf(graph, "[ label = \"%s\" ];\n", comm_op);
+    fscanf(f, "%s", buf);
+    ac.exec = atof(buf);
+
+    if(ac.exec > max_time)
+      max_time = ac.exec;
+
+    fscanf(f, "%s", buf);
+    ac.time = atof(buf);
+    fscanf(f, "%s", buf);
+    ac.module = buf;
+    if(!strcmp(comm_op, "get")){
+      ops[ atoi(info) ][ atoi(to) ].get++;
+      numgets++;
+      fprintf(timeline, "<rect x = \"%d\" y = \"%d\" width = \"%d\" \
+                         height = \"20\" style = \"stroke-width:2;fill:black\"/>\n",
+                         (int)((750 * ac.time) / end_time), cl * (ac.from + 1) - 20,
+                         (int)((ac.exec / end_time) * 750) + 1);
+
+      if(ac.from < ac.to)
+        fprintf(timeline, "<line x1=\"%d\" y1 = \"%d\" x2 = \"%d\" y2 = \"%d\" \
+                          style=\"stroke:grey;stroke-width:1\"/>\n",
+                          (int)((750 * ac.time) / end_time), cl * (ac.from + 1),
+                          (int)((750 * ac.time) / end_time), cl * (ac.to + 1));
+      else
+        fprintf(timeline, "<line x1=\"%d\" y1 = \"%d\" x2 = \"%d\" y2 = \"%d\" \
+                          style=\"stroke:grey;stroke-width:1\"/>\n",
+                          (int)((750 * ac.time) / end_time), cl * (ac.from + 1) - 20,
+                          (int)((750 * ac.time) / end_time), cl * (ac.to + 1));
+
+
+    }
+    else{
+      ops[ atoi(info) ][ atoi(to) ].put++;
+      numputs++;
+      fprintf(timeline, "<rect x = \"%d\" y = \"%d\" width = \"%d\" \
+                         height = \"20\" style = \"fill:black\"/>\n",
+                         (int)((750 * ac.time) / end_time), cl * (ac.from + 1),
+                         (int)((ac.exec / end_time) * 750) + 1);
+      if(ac.from > ac.to)
+        fprintf(timeline, "<line x1=\"%d\" y1 = \"%d\" x2 = \"%d\" y2 = \"%d\" \
+                          style=\"stroke:grey;stroke-width:1\"/>\n",
+                          (int)((750 * ac.time) / end_time), cl * (ac.from + 1),
+                          (int)((750 * ac.time) / end_time), cl * (ac.to + 1));
+      else
+        fprintf(timeline, "<line x1=\"%d\" y1 = \"%d\" x2 = \"%d\" y2 = \"%d\" \
+                          style=\"stroke:grey;stroke-width:1\"/>\n",
+                          (int)((750 * ac.time) / end_time), cl * (ac.from + 1) + 20,
+                          (int)((750 * ac.time) / end_time), cl * (ac.to + 1));
+
     }
   }
-
   fclose(f);
   fprintf(graph, "}\n");
   fclose(graph);
+
   fprintf(matrix, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
   fprintf(matrix, "<svg style=\"background: white\" version = \"1.1\"\n");
   fprintf(matrix, "baseProfile=\"full\"\n");
@@ -66,6 +131,7 @@ int main(int argc, char** argv)
   fprintf(matrix, "xmlns:xlink = \"http://www.w3.org/1999/xlink\" \n");
   fprintf(matrix, "xmlns:ev = \"http://www.w3.org/2001/xml-events\" \n");
   fprintf(matrix, "height = \"%dpx\" width = \"%dpx\">", cl * (numlocs + 1), cl * numlocs * 2);
+
   for(i = 0; i < numlocs; ++i){
     // Senders tags
     fprintf(matrix, "<text x = \"%d\" y = \"%d\" \
@@ -77,6 +143,15 @@ int main(int argc, char** argv)
                     fill = \"black\" font-family = \"arial\"> loc%d </text>\n",
                     cl / 2, cl * (i + 1) + cl / 3,
                     cl / 2, cl * (i + 1) + cl / 3, i);
+    // locale lines
+    fprintf(timeline, "<line x1=\"%dpx\" y1 = \"%dpx\" x2 = \"%dpx\" y2 = \"%dpx\" \
+                    style=\"stroke:black;stroke-width:1\"/>\n",
+                    50, cl * (i + 1), 750, cl * (i + 1));
+    // locale lines tags
+    fprintf(timeline, "<text x = \"%d\" y = \"%d\" \
+                    fill = \"black\" font-family = \"arial\"> loc%d </text>\n",
+                    20, cl * (i + 1) + 6, i);
+
 
     for(j = 0; j < numlocs; ++j){
 
@@ -128,31 +203,8 @@ int main(int argc, char** argv)
                       cl / 2+ cl* i + 5 ,cl / 2 + 5 + cl * numlocs);
 
     }
-/*
-  fprintf(matrix, "<polygon points=\"%d,%d %d,%d, %d,%d\" \
-                  style = \"fill:rgb(100,255,100);stroke:black;stroke-width:1\"/>\n",
-                  cl + cl * numlocs + 5, cl / 2 + cl * (numlocs - (numlocs - 1)) + 5,
-                  cl + cl * numlocs + 5, cl / 2 + cl * (2)+ 5,
-                  cl + cl * (numlocs + 1) + 5, cl / 2 + cl * (numlocs - (numlocs - 1)) + 5,
-                  col, col);
 
-  fprintf(matrix, "<polygon points=\"%d,%d %d,%d, %d,%d\" \
-                  style = \"fill:rgb(255,40,40);stroke:black;stroke-width:1\"/>\n",
-                  2 * cl + cl * numlocs + 5, cl / 2 + cl * 2 + 5,
-                  cl + cl * numlocs + 5, cl / 2 + cl * 2 + 5,
-                  cl + cl * (numlocs + 1) + 5, cl / 2 + cl * (numlocs - (numlocs - 1)) + 5,
-                  col, col);
-
-  fprintf(matrix, "<text x = \"%d\" y = \"%d\" \
-                  style=\"fill:black;font-family:arial\"> %d </text>\n",
-                  cl + cl * numlocs + 5, cl / 2 + cl * (numlocs - (numlocs - 1)) + 5,
-                  20);
-
-  fprintf(matrix, "<text x = \"%d\" y = \"%d\" \
-                  style = \"fill:black;font-family:arial\"> %d </text>\n",
-                  cl / 2 + cl * i + cl / 4 + cl / 2,
-                  cl + cl * j + cl / 4 + cl / 6, 5);
-*/
+  fprintf(timeline, "</svg>");
   fprintf(matrix, "</svg>");
   fclose(matrix);
   for(i = 0; i < numlocs; ++i)
